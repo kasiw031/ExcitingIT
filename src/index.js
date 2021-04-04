724b1495579095791e01163662409712e912d12c
Kamila <kasiw031@gmail.com>
2021-04-04 15:33:46 +0200

    Added: arrows

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import arrow from './arrow.png';
 
-function Title(props) {
-   return(
-       <h1>{props.value}</h1>
-   )
-}
+// onmouseenter	The event occurs when the pointer is moved 
+// onto an element
+// onmouseleave	The event occurs when the pointer is moved 
+// out of an element
 
 function ListItem(props) {
     return(
-        <h2><a href="#">{props.value}</a></h2>
-    )
-}
-
-function Btn(props) {
-    return(
-        <button class="contact-btn">{props.value}</button>
+        <div class="listItem-container">
+            <img class="arrow-img" src={arrow}></img>
+            <h2><a href="#">{props.value}</a></h2>
+        </div>
     )
 }
 
 function MainContainer(props) {
     return(
         <div class="main-container">
-            <Title value="EXCITING IT"/>
+            <h1>EXCITING IT</h1>
             <ListItem value="LinkedIn"/>
             <ListItem value="github"/>
             <ListItem value="company info"/>
-            <Btn value="CONTACT"/>
+            <button class="contact-btn">CONTACT</button>
         </div>
     )
 }
